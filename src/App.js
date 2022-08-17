1f26e15af139ba859becba427b393f62b494e029
Jair montenegro <jairmontenegrof@gmail.com>
2022-08-16 21:38:12 -0300

    start-project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './sass/styles.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <div id="quote-box">
+        <div id="text">
+      <h1>Random quote</h1>
+      <p> Todas las penas pueden soportarse si las ponemos en una historia o contamos una historia sobre ellas</p>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
