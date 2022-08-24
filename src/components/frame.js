955e0b116b4dcf50b1894132454a8195d06c4b73
Jair montenegro <jairmontenegrof@gmail.com>
2022-08-24 19:19:53 -0300

    new quote

--- src/components/frame.js
+++ src/components/frame.js
@@ -14,22 +14,15 @@ function Frame(props) {
   return (
     <div id="quote-box">
       <header>PSICOLOGIA Y CULTURA</header>
-      <p id="text">
-        Las redes sociales son una trampa 'El diálogo real no es hablar con
-        gente que piensa lo mismo que tú. Las redes sociales no enseñan a
-        dialogar porque es tan fácil evitar la controversia… Mucha gente usa las
-        redes sociales no para unir, no para ampliar sus horizontes, sino al
-        contrario, para encerrarse en lo que llamo zonas de confort, donde el
-        único sonido que oyen es el eco de su voz, donde lo único que ven son
-        los reflejos de su propia cara. Las redes son muy útiles, dan servicios
-        muy placenteros, pero son una trampa'
-      </p>
+      <p id="text">"Ninguna pérdida es ajena a la problematica social." </p>
       <img
         className="img-inpsac"
         src={require(`../img/${props.image}.png`)}
         alt="img-inpsac"
       />
-      <p id="author">Zygmunt Bauman Filósofo polaco</p>
+      <p id="author">
+        Sarah Cohen <br /> La niñez cautiva
+      </p>
       <Share />
     </div>
   );
