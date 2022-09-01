a30b45f37972f4c69f522cba942f3ba1b3792225
Jair montenegro <jairmontenegrof@gmail.com>
2022-08-31 22:45:17 -0300

    fixing btn tweet

--- src/components/social.js
+++ src/components/social.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Socialmedia({ isTweet, text, icon }) {
+function Socialmedia({ isTweet, text, icon, quote }) {
   return (
     <a
       id={isTweet ? "tweet-quote" : "share-quote"}
-      href={`https://twiter.com/intent/tweet?text=hello`}
+      href={`https://twiter.com/intent/tweet?text=${quote}sigueme en @inspac`}
       target="_blank"
       rel="noopener noreferrer"
       className={isTweet ? "btn-twitter" : "btn-instagram"}
