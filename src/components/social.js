7628b381b7cf141e20616340cdb4da309051aa99
Jair montenegro <jairmontenegrof@gmail.com>
2022-09-05 22:06:44 -0300

    fixing some problems

--- src/components/social.js
+++ src/components/social.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-function Socialmedia({ isTweet, text, icon, quote }) {
+function Socialmedia({ isTweet, text, icon }) {
+  let addUrl = "";
   return (
     <a
       id={isTweet ? "tweet-quote" : "share-quote"}
-      href={`https://twiter.com/intent/tweet?text=eninstagram@inpsac`}
+      href={`https://twiter.com/intent/tweet?text=${addUrl}`}
       target="_blank"
       rel="noopener noreferrer"
       className={isTweet ? "btn-twitter" : "btn-instagram"}
